6b09895430b101b49e2f6fd2065feb711622b3f8
Leandrohsu <leandrohsu@outlook.com>
2025-04-25 16:49:17 -0300

    Atualizando o teste

--- teste/src/test/java/app/service/test/CalculoServiceTest.java
+++ teste/src/test/java/app/service/test/CalculoServiceTest.java
@@ -104,82 +104,96 @@ public class CalculoServiceTest {
 	
 	
     @Test
+    @DisplayName("Cena 06 - Lista normal")
     void testSoma_ListaNormal() {
         List<Integer> lista = Arrays.asList(1, 2, 3, 4);
         assertEquals(10, calculoService.soma(lista));
     }
 
     @Test
+    @DisplayName("Cena 07 - Lista com zeros")
     void testSoma_ListaComZeros() {
         List<Integer> lista = Arrays.asList(0, 0, 0);
         assertEquals(0, calculoService.soma(lista));
     }
 
     @Test
+    @DisplayName("Cena 08 - Lista com Negativos")
     void testSoma_ListaComNegativos() {
         List<Integer> lista = Arrays.asList(-1, -2, -3, -4);
         assertEquals(-10, calculoService.soma(lista));
     }
 
     @Test
+    @DisplayName("Cena 09 - Lista com Null")
     void testSoma_ListaComNull() {
         List<Integer> lista = Arrays.asList(1, null, 3);
         assertThrows(RuntimeException.class, () -> calculoService.soma(lista));
     }
 
     @Test
+    @DisplayName("Cena 10 - Lista Normal")
     void testMedia_ListaNormal() {
         List<Integer> lista = Arrays.asList(1, 2, 3, 4);
         assertEquals(2.5, calculoService.media(lista));
     }
 
     @Test
+    @DisplayName("Cena 11 - Lista com Zeros")
     void testMedia_ListaComZeros() {
         List<Integer> lista = Arrays.asList(0, 0, 0);
         assertEquals(0, calculoService.media(lista));
     }
 
     @Test
+    @DisplayName("Cena 12 - Lista com Negativos")
     void testMedia_ListaComNegativos() {
         List<Integer> lista = Arrays.asList(-1, -2, -3, -4);
         assertEquals(-2.5, calculoService.media(lista));
     }
 
     @Test
+    @DisplayName("Cena 13 - Lista Vazia")
     void testMedia_ListaVazia() {
         assertThrows(ArithmeticException.class, () -> calculoService.media(Arrays.asList()));
     }
 
     @Test
+    @DisplayName("Cena 14 - Lista Com Elementos Impares")
     void testMediana_ListaComElementosÍmpares() {
         List<Integer> lista = Arrays.asList(1, 3, 7, 9, 11);
         assertEquals(7, calculoService.mediana(lista));
     }
 
     @Test
+    @DisplayName("Cena 15 - Lista Com Elementos Pares")
     void testMediana_ListaComElementosPares() {
         List<Integer> lista = Arrays.asList(1, 3, 7, 9);
         assertEquals(5, calculoService.mediana(lista));
     }
 
     @Test
+    @DisplayName("Cena 16 - Lista Ordenada")
     void testMediana_ListaOrdenada() {
         List<Integer> lista = Arrays.asList(10, 20, 30, 40);
         assertEquals(25, calculoService.mediana(lista));
     }
 
     @Test
+    @DisplayName("Cena 17 - Lista Desordenada")
     void testMediana_ListaDesordenada() {
         List<Integer> lista = Arrays.asList(40, 10, 30, 20);
         assertEquals(25, calculoService.mediana(lista));
     }
 
     @Test
+    @DisplayName("Cena 18 - Lista Vazia")
     void testMediana_ListaVazia() {
         assertThrows(IllegalArgumentException.class, () -> calculoService.mediana(Arrays.asList()));
     }
 
     @Test
+    @DisplayName("Cena 19 - Lista com Null")
     void testMediana_ListaNula() {
         assertThrows(IllegalArgumentException.class, () -> calculoService.mediana(null));
     }
